3f33e16c8463710e2166bb1fcb5b97d1e5464051
dhairya-motta <dhairyamotta@gmail.com>
2026-01-31 23:49:32 +0530

    Initialize Spring Boot application with scheduling support

--- src/main/java/com/project/SaaS/subscription_billing_api/SaaSSubscriptionAndBillingApiApplication.java
+++ src/main/java/com/project/SaaS/subscription_billing_api/SaaSSubscriptionAndBillingApiApplication.java
@@ -2,8 +2,10 @@ package com.project.SaaS.subscription_billing_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SaaSSubscriptionAndBillingApiApplication {
 
 	public static void main(String[] args) {
