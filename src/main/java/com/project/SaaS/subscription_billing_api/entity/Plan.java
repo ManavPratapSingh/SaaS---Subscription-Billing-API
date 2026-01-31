f79e30240c734dec3186b400bdf26c5bcc1fe880
PKG-boii <piyush.24bcs10332@sst.scaler.com>
2026-01-31 22:20:54 +0530

    Add files via upload
    Addwd plan

---
+++ src/main/java/com/project/SaaS/subscription_billing_api/entity/Plan.java
@@ -0,0 +1,36 @@
+package com.project.SaaS.subscription_billing_api.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "plans")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Plan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Plan name is required")
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @NotNull(message = "Price is required")
+    @Min(value = 0, message = "Price must be positive")
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @NotNull(message = "Duration is required")
+    @Min(value = 1, message = "Duration must be at least 1 day")
+    @Column(name = "duration_days", nullable = false)
+    private Integer durationDays;
+}
