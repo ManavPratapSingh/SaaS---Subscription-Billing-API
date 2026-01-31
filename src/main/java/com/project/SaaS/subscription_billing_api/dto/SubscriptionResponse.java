b800049cb74589c139cf24f17906dd99407802d0
ManavPratap <manav.24bcs10126@sst.scaler.com>
2026-02-01 01:42:56 +0530

    added serialization to DTOs

--- src/main/java/com/project/SaaS/subscription_billing_api/dto/SubscriptionResponse.java
+++ src/main/java/com/project/SaaS/subscription_billing_api/dto/SubscriptionResponse.java
@@ -6,13 +6,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class SubscriptionResponse {
+public class SubscriptionResponse implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private Long id;
 
